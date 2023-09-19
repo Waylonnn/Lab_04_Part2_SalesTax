47ef912b4ae1947f01489bbbcfcf1f45aac18c67
Waylon <weese.619547@swl.k12.oh.us>
2023-09-19 09:09:58 -0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double total;
+        int price;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter price of item: ");
+        price = scan.nextInt();
+        total = (price * 1.05);
+        System.out.println("New price after sales tax: $" + total);
     }
 }
